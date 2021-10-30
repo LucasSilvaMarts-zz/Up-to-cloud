6b727a22b76ac63a53e81b3580947c4c170d57c7
Lucas Silva <martssilvapro@gmail.com>
2021-10-30 12:40:23 -0300

    setup mongodb connection

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3001;
+
+/*
+  Database setup
+*/
+mongoose.connect('mongodb://localhost:27017/upload', {
+  useNewUrlParser: true,
+})
 
 app.use(require('./routes'));
 app.use(express.urlencoded({ extended: true })); // Facilita envio de arquivos
