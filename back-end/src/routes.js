b53e0b4f7aea8f6cd4b5f405f69a05a18f53e33f
Lucas Silva <martssilvapro@gmail.com>
2021-10-30 12:15:15 -0300

    configura multer para upload local

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,5 +1,9 @@
 const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
-routes.get('/', (req, res) => res.json({ hello: '👌' }));
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+  res.json({ hello: '👌' });
+});
 
 module.exports = routes;
