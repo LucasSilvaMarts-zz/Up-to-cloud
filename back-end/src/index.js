2ee94681dcd60bb73793cd5acf7b0563d69e5e24
Lucas Silva <martssilvapro@gmail.com>
2021-10-30 14:53:04 -0300

    app finalizado

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -3,19 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
+
 const app = express();
 const port = 3001;
 
 /*
   Database setup
 */
-mongoose.connect('mongodb://localhost:27017/upload', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
 });
 
 app.use(require('./routes'));
 app.use(express.urlencoded({ extended: true })); // Facilita envio de arquivos
 app.use(morgan('dev'));
-
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+);
 
 app.listen(port, () => console.log(`app listening on ${port} port!`));
