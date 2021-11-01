6b5d94f579ed422f6ef7d428203f2faf09539699
Lucas Silva <martssilvapro@gmail.com>
2021-11-01 13:56:40 -0300

    Estilizaçao do FileList finalizada

--- front-end/src/components/FileList/index.js
+++ front-end/src/components/FileList/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import CircularProgressbar from 'react-circular-progressbar';
+import { CircularProgressbar } from 'react-circular-progressbar';
 
 import { Container, FileInfo, Preview } from './styles';
+import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
 
 const FileList = () => {
   return (
     <Container>
       <li>
         <FileInfo>
-          <Preview src="" />
+          <Preview src="http://localhost:3001/files/960daa6d034be22898f50647a8c54c24-wallpaperflare.com_wallpaper.jpg" />
           <div>
             <strong>profile.png</strong>
             <span>64kb <button onClick={() => {}}>Excluir</button></span>
@@ -24,6 +25,17 @@ const FileList = () => {
             strokeWidth={10}
             percentage={60}
           />
+
+          <a
+            href="http://localhost:3001/files/960daa6d034be22898f50647a8c54c24-wallpaperflare.com_wallpaper.jpg"
+            target="_blank"
+            rel="nooper noreferrer"
+          >
+            <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+          </a>
+
+          <MdCheckCircle size={24} color="#78e5d5" />
+          <MdError size={24} color="#e57878" />
         </div>
       </li>
     </Container>
